8ab56a84fa6d27d21101f673efa4fe2ae02f4569
Phan Hoàng Long <long.ph@3si.com.vn>
2021-10-25 07:56:26 +0700

    PHLong_countDownWinform

---
+++ CountDownWinform/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace CountDownWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.minuteBox = new System.Windows.Forms.ComboBox();
+            this.secondsBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // minuteBox
+            // 
+            this.minuteBox.FormattingEnabled = true;
+            this.minuteBox.Location = new System.Drawing.Point(195, 330);
+            this.minuteBox.Name = "minuteBox";
+            this.minuteBox.Size = new System.Drawing.Size(121, 24);
+            this.minuteBox.TabIndex = 0;
+            // 
+            // secondsBox
+            // 
+            this.secondsBox.FormattingEnabled = true;
+            this.secondsBox.Location = new System.Drawing.Point(322, 330);
+            this.secondsBox.Name = "secondsBox";
+            this.secondsBox.Size = new System.Drawing.Size(121, 24);
+            this.secondsBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 310);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Type Minutes :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Type seconds:";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(195, 373);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(121, 50);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(322, 373);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(121, 50);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(206, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(237, 91);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "00:00";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 458);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondsBox);
+            this.Controls.Add(this.minuteBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox minuteBox;
+        private System.Windows.Forms.ComboBox secondsBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CountDownWinform/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDownWinform
+{
+    public partial class Form1 : Form
+    {
+        private int totalSeconds;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                this.minuteBox.Items.Add(i.ToString());
+                this.secondsBox.Items.Add(i.ToString());
+            }
+            this.minuteBox.SelectedIndex = 59;
+            this.secondsBox.SelectedIndex = 59;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.buttonStart.Enabled = false;
+            this.buttonStop.Enabled = true;
+
+            int muinutes = int.Parse(this.minuteBox.SelectedItem.ToString());
+            int seconds = int.Parse(this.secondsBox.SelectedItem.ToString());
+
+            totalSeconds = (muinutes * 60) + seconds;
+
+            this.timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.buttonStop.Enabled = false;
+            this.buttonStart.Enabled = true;
+
+            totalSeconds = 0;
+            this.timer1.Enabled = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (totalSeconds > 0)
+            {
+                totalSeconds--;
+                int minutes = totalSeconds / 60;
+                int seconds = totalSeconds - (minutes * 60);
+
+                this.label3.Text = minutes.ToString() + ":" + seconds.ToString();
+                if(seconds < 10)
+                {
+                    this.label3.Text = minutes.ToString() + ":0" + seconds.ToString();
+                }
+                if(minutes < 10)
+                {
+                    this.label3.Text = "0" + minutes.ToString() + ":" + seconds.ToString(); 
+                }
+                if((minutes < 10) && (seconds < 10))
+                {
+                    this.label3.Text = "0" + minutes.ToString() + ":0" + seconds.ToString();
+                }
+            }
+            else
+            {
+                this.timer1.Stop();
+                MessageBox.Show("HAPPY NEW YEAR");
+            }
+        }
+    }
+}
